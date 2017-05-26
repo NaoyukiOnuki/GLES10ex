51246d83b08310e2418dc4fe3cf9b5eaf72504d3
onuki <rcpxg844@yahoo.co.jp>
2017-05-26 18:44:00 +0900

    use index buffer

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/Cube.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/Cube.java
@@ -3,51 +3,61 @@ package jp.ac.titech.itpro.sdl.gles10ex;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
 public class Cube implements SimpleRenderer.Obj {
 
     private FloatBuffer vbuf;
+    private FloatBuffer nbuf;
+    private ShortBuffer ibuf1;
+    private ShortBuffer ibuf2;
     private float x, y, z;
 
     public Cube(float s, float x, float y, float z) {
         float[] vertices = {
-                // left
                 -s, -s, -s,
                 -s, -s, s,
                 -s, s, -s,
                 -s, s, s,
-                // right
                 s, -s, -s,
                 s, -s, s,
                 s, s, -s,
-                s, s, s,
-                // bottom
-                -s, -s, -s,
-                s, -s, -s,
-                -s, -s, s,
-                s, -s, s,
-                // top
-                -s, s, -s,
-                s, s, -s,
-                -s, s, s,
-                s, s, s,
-                // back
-                -s, -s, -s,
-                -s, s, -s,
-                s, -s, -s,
-                s, s, -s,
-                // front
-                -s, -s, s,
-                -s, s, s,
-                s, -s, s,
                 s, s, s
         };
         vbuf = ByteBuffer.allocateDirect(vertices.length * 4)
                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
         vbuf.put(vertices);
         vbuf.position(0);
+        float[] normals = {
+                -s, -s, -s,
+                -s, -s, s,
+                -s, s, -s,
+                -s, s, s,
+                s, -s, -s,
+                s, -s, s,
+                s, s, -s,
+                s, s, s
+        };
+        nbuf = ByteBuffer.allocateDirect(normals.length * 4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        nbuf.put(normals);
+        nbuf.position(0);
+        short[] indices1 = {
+                0,1,3,2,6,4,5,1
+        };
+        ibuf1 = ByteBuffer.allocateDirect(indices1.length * Short.SIZE)
+                .order(ByteOrder.nativeOrder()).asShortBuffer();
+        ibuf1.put(indices1);
+        ibuf1.position(0);
+        short[] induces2 = {
+                7,1,3,2,6,4,5,1
+        };
+        ibuf2 = ByteBuffer.allocateDirect(induces2.length * Short.SIZE)
+                .order(ByteOrder.nativeOrder()).asShortBuffer();
+        ibuf2.put(induces2);
+        ibuf2.position(0);
         this.x = x;
         this.y = y;
         this.z = z;
@@ -55,31 +65,12 @@ public class Cube implements SimpleRenderer.Obj {
 
     public void draw(GL10 gl) {
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY );
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vbuf);
+        gl.glNormalPointer( GL10.GL_FLOAT , 0 , nbuf );
 
-        // left
-        gl.glNormal3f(-1, 0, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
-
-        // right
-        gl.glNormal3f(1, 0, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 4, 4);
-
-        // bottom
-        gl.glNormal3f(0, -1, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 8, 4);
-
-        // top
-        gl.glNormal3f(0, 1, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 12, 4);
-
-        // rear
-        gl.glNormal3f(0, 0, -1);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 16, 4);
-
-        // front
-        gl.glNormal3f(0, 0, 1);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 20, 4);
+        gl.glDrawElements( GL10.GL_TRIANGLE_FAN , ibuf1.remaining() , GL10.GL_UNSIGNED_SHORT, ibuf1);
+        gl.glDrawElements( GL10.GL_TRIANGLE_FAN , ibuf2.remaining() , GL10.GL_UNSIGNED_SHORT, ibuf2);
     }
     @Override
     public float getX() {
--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/Pyramid.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/Pyramid.java
@@ -3,12 +3,16 @@ package jp.ac.titech.itpro.sdl.gles10ex;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.opengles.GL10;
 
 public class Pyramid implements SimpleRenderer.Obj {
 
     private FloatBuffer vbuf;
+    private FloatBuffer nbuf;
+    private ShortBuffer bottombuf;
+    private ShortBuffer topbuf;
     private float x, y, z;
 
     public Pyramid(float s, float x, float y, float z) {
@@ -18,27 +22,40 @@ public class Pyramid implements SimpleRenderer.Obj {
                 s, 0, -s,
                 -s, 0, s,
                 s, 0, s,
-                // left
-                -s, 0, -s,
+                // top
                 0, s, 0,
-                -s, 0, s,
-                // right
-                s, 0, -s,
-                0, s, 0,
-                s, 0, s,
-                // back
-                -s, 0, -s,
-                0, s, 0,
-                s, 0, -s,
-                // front
-                -s, 0, s,
-                0, s, 0,
-                s, 0, s
         };
         vbuf = ByteBuffer.allocateDirect(vertices.length * 4)
                 .order(ByteOrder.nativeOrder()).asFloatBuffer();
         vbuf.put(vertices);
         vbuf.position(0);
+        float[] normals = {
+                // bottom
+                -1, -1, -1,
+                1, -1, -1,
+                -1, -1, 1,
+                1, -1, 1,
+                // top
+                0, 1, 0,
+        };
+        nbuf = ByteBuffer.allocateDirect(normals.length * 4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        nbuf.put(normals);
+        nbuf.position(0);
+        short[] bottomindices = {
+                0,1,3,2
+        };
+        bottombuf = ByteBuffer.allocateDirect(bottomindices.length * Short.SIZE)
+                .order(ByteOrder.nativeOrder()).asShortBuffer();
+        bottombuf.put(bottomindices);
+        bottombuf.position(0);
+        short[] topindices = {
+                4,0,1,3,2
+        };
+        topbuf = ByteBuffer.allocateDirect(topindices.length * Short.SIZE)
+                .order(ByteOrder.nativeOrder()).asShortBuffer();
+        topbuf.put(topindices);
+        topbuf.position(0);
         this.x = x;
         this.y = y;
         this.z = z;
@@ -47,27 +64,12 @@ public class Pyramid implements SimpleRenderer.Obj {
     @Override
     public void draw(GL10 gl) {
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_NORMAL_ARRAY );
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vbuf);
+        gl.glNormalPointer( GL10.GL_FLOAT , 0 , nbuf );
 
-        // bottom
-        gl.glNormal3f(0, -1, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, 4);
-
-        // left
-        gl.glNormal3f(-1, 1, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 4, 3);
-
-        // right
-        gl.glNormal3f(1, 1, 0);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 7, 3);
-
-        // back
-        gl.glNormal3f(0, 1, -1);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 10, 3);
-
-        // front
-        gl.glNormal3f(0, 1, 1);
-        gl.glDrawArrays(GL10.GL_TRIANGLES, 13, 3);
+        gl.glDrawElements( GL10.GL_TRIANGLE_FAN , bottombuf.remaining() , GL10.GL_UNSIGNED_SHORT, bottombuf);
+        gl.glDrawElements( GL10.GL_TRIANGLE_FAN , topbuf.remaining() , GL10.GL_UNSIGNED_SHORT, topbuf);
     }
 
     @Override
